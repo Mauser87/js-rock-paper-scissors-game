6beeef235595b365c1a5dc4916fd90489d681942
Mauser87 <dfmx666@wp.pl>
2019-09-06 13:18:26 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,120 @@
+let userScore = 0;
+let computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_p = document.querySelector(".result > p");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+const computerChoice_img = document.getElementById("comp-img")
+const userChoice_img = document.getElementById("user-img")
+const array_img = ['pictures/rsmall.png ',
+    'pictures/psmall.png',
+    'pictures/ssmall.png'];
+
+
+
+function getComputerChoice() {
+    const choices = ['r', 'p', 's']
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+function convertToWord(letter) {
+    if (letter === "r") return "Rock";
+    if (letter === "p") return "Paper";
+    return "Scissors";
+}
+
+function changeUserImg(userChoice) {
+    if (userChoice === "r") { return userChoice_img.src = array_img[0] };
+    if (userChoice === "p") { return userChoice_img.src =array_img[1] };
+    if (userChoice === "s") { return userChoice_img.src = array_img[2] };
+}
+
+function changeComputerImg(computerChoice) {
+    if (computerChoice === "r") { return computerChoice_img.src = array_img[0] };
+    if (computerChoice === "p") { return computerChoice_img.src = array_img[1] };
+    if (computerChoice === "s") { return computerChoice_img.src = array_img[2] };
+}
+
+
+
+function win(userChoice, computerChoice) {
+    const smallUserWord = "user".fontsize(3).sup();
+    const smallComputerWord = "comp".fontsize(3).sup();
+    const userChoice_div = document.getElementById(userChoice);
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} beats ${convertToWord(computerChoice)}${smallComputerWord}  You win! `;
+    userChoice_div.classList.add("green-glow");
+    setTimeout(() => userChoice_div.classList.remove("green-glow"), 500);
+
+}
+
+
+function lose(userChoice, computerChoice) {
+    const smallUserWord = "user".fontsize(3).sup();
+    const smallComputerWord = "comp".fontsize(3).sup();
+    const userChoice_div = document.getElementById(userChoice);
+    computerScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} loses to ${convertToWord(computerChoice)}${smallComputerWord}  You lost! `;
+    userChoice_div.classList.add("red-glow");
+    setTimeout(() => userChoice_div.classList.remove("red-glow"), 500);
+
+}
+
+
+function draw(userChoice, computerChoice) {
+    const smallUserWord = "user".fontsize(3).sup();
+    const smallComputerWord = "comp".fontsize(3).sup();
+    const userChoice_div = document.getElementById(userChoice);
+    result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} equal ${convertToWord(computerChoice)}${smallComputerWord}  It's a draw! `;
+    userChoice_div.classList.add("gray-glow");
+    setTimeout(() => userChoice_div.classList.remove("gray-glow"), 500);
+
+}
+
+
+
+
+function game(userChoice) {
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice) {
+        case "rs":
+        case "pr":
+        case "sp":
+            win(userChoice, computerChoice);
+            changeUserImg(userChoice);
+            changeComputerImg(computerChoice);
+            break;
+        case "rp":
+        case "ps":
+        case "sr":
+            lose(userChoice, computerChoice);
+            changeUserImg(userChoice);
+            changeComputerImg(computerChoice);
+            break;
+        case "rr":
+        case "pp":
+        case "ss":
+            draw(userChoice, computerChoice);
+            changeUserImg(userChoice);
+            changeComputerImg(computerChoice);
+            break;
+    }
+}
+
+
+
+function main() {
+    rock_div.addEventListener('click', () => game("r"));
+    paper_div.addEventListener('click', () => game("p"));
+    scissors_div.addEventListener('click', () => game("s"));
+};
+
+main();
